df4fa03360a9d4f27a17226d86f70f171834de8c
Stefan Stölzle <stoe@github.com>
2016-08-30 14:26:15 +0200

    Replace jade w/ pug to fix snyk vulnerability

--- app.js
+++ app.js
@@ -17,13 +17,15 @@ app.set('etag', false);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
